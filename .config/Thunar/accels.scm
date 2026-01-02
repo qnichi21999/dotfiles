1cd49ba3c7327789e5c9e8ded8f0d04da0c49b4f
qnichi21999 <21999german@gmail.com>
2026-01-02 13:01:18 +0300

    Update config

--- .config/Thunar/accels.scm
+++ .config/Thunar/accels.scm
@@ -18,6 +18,7 @@
 ; (gtk_accel_path "<Actions>/ThunarWindow/view-side-pane-menu" "")
 ; (gtk_accel_path "<Actions>/ThunarStatusBar/toggle-size-in-bytes" "")
 ; (gtk_accel_path "<Actions>/ThunarWindow/switch-previous-tab" "<Primary>Page_Up")
+; (gtk_accel_path "<Actions>/ThunarBookmarks/f3121674e1e7720844a704b11ddc53cb" "")
 ; (gtk_accel_path "<Actions>/ThunarActionManager/open" "<Primary>o")
 ; (gtk_accel_path "<Actions>/ThunarStandardView/sort-ascending" "")
 ; (gtk_accel_path "<Actions>/ThunarWindow/toggle-split-view" "F3")
